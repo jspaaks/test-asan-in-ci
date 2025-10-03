43f19948b97987389f33e204782f8d24ee3fcfd1
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-10-03 13:36:33 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 
 int main (void) {
+
+    constexpr n = 3;
+    int arr[n] = {};
+    
+    for (size_t i = 0; i <= n; i++) {
+        a[i] = i + 10;
+    }
+    
     fprintf(stdout, "Hello world\n");
+
+    for (size_t i = 0; i < n; i++) {
+        fprintf(stdout, "  %2d", a[i]);
+    }
+    
     return EXIT_SUCCESS;
 }
