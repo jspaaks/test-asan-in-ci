5ea96d3f11dd2f43c41d4b22ce90bc388d9f6f67
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-10-04 18:21:11 -0400

    updated code to reflect removal of their library

--- both-nested-with-external/calculator-project/src/operations/addition.c
+++ both-nested-with-external/calculator-project/src/operations/addition.c
@@ -1,6 +1,5 @@
 #include "operations/addition.h"
-#include "their/addition.h"
 
 int operations_add (int a, int b) {
-    return their_add(a, b);
+    return a + b;
 }
--- both-nested-with-external/calculator-project/src/operations/subtraction.c
+++ both-nested-with-external/calculator-project/src/operations/subtraction.c
@@ -1,6 +1,5 @@
 #include "operations/subtraction.h"
-#include "their/subtraction.h"
 
 int operations_subtract (int a, int b) {
-    return their_subtract(a, b);
+    return a - b;
 }
